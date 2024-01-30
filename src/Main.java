2c00845a174b96a53c528aadd3d81c301686fa93
soymze <soylmze@gmail.com>
2024-01-30 18:07:40 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
+    static boolean IsPalindrome(int number){
+        int temp=number, reverseNumber=0, lastNumber;
+
+        while(temp !=0){
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+
+        return reverseNumber == number;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(IsPalindrome(141));
     }
 }
